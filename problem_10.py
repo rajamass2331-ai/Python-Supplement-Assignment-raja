6986e883b417ecbf77ef00f7aca04e2601438c28
raja s <rajamass4512@gmail.com>
2026-01-30 15:41:03 +0530

    modified problem_10.py

--- problem_10.py
+++ problem_10.py
@@ -2,8 +2,11 @@
 # Find and fix the error
 
 numbers = [1, 2, 2, 3, 4, 4, 5]
-unique = []
+unique_numbers = []
+
 for num in numbers:
-    if num not in unique:
-       unique.append(num)
-print(f"Unique numbers:{unique}")
+    if num not in unique_numbers:
+        unique_numbers.append(num)
+
+print(f"Without duplicates: {unique_numbers}")
+
