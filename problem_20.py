f04eda71bf5f9585c1cddbc954284d77845099b0
raja s <rajamass4512@gmail.com>
2026-01-30 16:06:30 +0530

    modified problem_20.py

--- problem_20.py
+++ problem_20.py
@@ -1,8 +1,8 @@
 # Problem 20: Find common elements in two lists
 # Find and fix the error
 
-list1 = [1, 2, 3, 4, 5]
-list2 = [4, 5, 6, 7, 8]
+list1=[1, 2, 3, 4, 5]
+list2=[4, 5, 6, 7, 8]
 common = []
 for item in list1:
     if item in list2:
